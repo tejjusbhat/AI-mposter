6bbc8d4b3a535e655346f81496e18f83bc4dc086
[uditi sharma] <[uditi1002@gmail.com]>
2023-09-17 11:35:27 +0530

    adds middle section to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,13 +16,38 @@ export const Home = (): JSX.Element => {
         </nav>
         <div className="label">
             <div className="find-the-imposter">
-                <h1>
-                    Find The
-                    Imposter
-                </h1>
+              <span className="find-the">
+                Find The
+                <br />
+                Imposter
+              </span>               
+              <SignInButton>
+                <button>Get Started</button>
+              </SignInButton>
             </div>
         </div>
       </section>
+
+      <section className="upper-middle-section">
+        <span>Don't know how to play?</span>
+      </section>
+
+      <section className="lower-middle-section">
+        <span>RULES</span>
+        <div className="rules">
+          <ul>
+            <li>Do not spam the chat.</li>
+            <li>Do not use multiple usernames.</li>
+            <li>Type in normal english.</li>
+            <li>Do not share personal information about yourself or others.</li>
+            <li>Do not personally attack or harass anyone in the chatroom.</li>
+            <li>Keep conversations on topic and relevant to the chat room.</li>
+            <li>Do not use any slurs or offensive language.</li>
+            <li>And lastly, remember there's a human behind the chat. Be kind.</li>
+          </ul>
+        </div>
+      </section>
+
     </main>
   );
 };
